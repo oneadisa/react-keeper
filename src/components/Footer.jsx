ccbeca17cff932ddd33bdba54485f335eaaea62d
Abdul Ibrahim <theoneadisa@gmail.com>
2021-10-07 23:43:03 +0100

    v-3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 function Footer() {
-  const year = new Date().getFullYear();
-  return (
-    <footer>
-      <p>Copyright ⓒ {year}</p>
-    </footer>
-  );
+    const year = new Date().getFullYear();
+    return ( <
+        footer >
+        <
+        p > Copyrightⓒ Abdul Ibrahim { year } < /p> <
+        /footer>
+    );
 }
 
 export default Footer;
